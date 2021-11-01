1506b10b78afe46e73ccb3c01c89885ebeee5489
17hvyxx <93510693+17hvyxx@users.noreply.github.com>
2021-11-01 16:35:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: '안녕하세요. 내가 하린 남친!',
+    introTitle: 'Annyeong Halinnnn Cute!',
     introDesc: 'Trái đất vốn lạ 
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương',
-    btnIntro: ':)) ㅇㅅㅇ lew lew',
+    btnIntro: ':)) lew lew',
     title: 'Chạy về nơi phía anh cánh tay này dài rộng lắm . Chạy về nơi phía anh , trái tim này cũng ấm lắm! 😙',
     desc: 'Chạy đi đâu nữa em , chỉ có beenn anh là nơi an toàn nhất rồi! Đừng đi đâu , chạy về nơi phía anh ♥♥♥ ',
-    btnYes: 'Dù có gì vẫn mãi yêu anh 사랑해 <33',
+    btnYes: 'Dù có gì vẫn mãi yêu anh <33',
     btnNo: 'Không, Em chỉ coi anh là bạn :3',
     question:'Trên thế giới này có hơn 8 tỷ nụ cười , nhưng đối với anh nụ cười của em mới là đẹp nhất <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Em sẽ yêu anh trọn đời , ngoan ngoãn để anh cưng chiều  <33333333',
-    mess: 'Anh biết mà 🥰. 너도 사랑해ㅐㅐㅐ 😘😘',
+    mess: 'Anh biết mà 🥰. Anh cũng iuu iem 😘😘',
     messDesc: 'Hẹn gặp em iu ở HANU ♥.',
     btnAccept: 'Okiiiii lun <3',
     messLink:  ' https://m.me/v.17thsept' ,
