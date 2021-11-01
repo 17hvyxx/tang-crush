2410538f8254c66e49effdf041dacf937d1f439a
17hvyxx <93510693+17hvyxx@users.noreply.github.com>
2021-11-01 16:05:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 const CONFIG = {
-    introTitle: 'Hii',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: ':)))',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙'
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -13,5 +15,6 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'Khum có' ,
+    messLink: 'https://www.facebook.com/v.17thsept/'
 }
+
