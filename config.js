81afff0f199485ddc52023f23886320c5498c02f
17hvyxx <93510693+17hvyxx@users.noreply.github.com>
2021-11-01 16:39:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,22 @@
 const CONFIG = {
-    introTitle: 'Annyeong Halinnnn Cute!',
-    introDesc:' Trái đất vốn lạ 
+    introTitle: 'Babe à!',
+    introDesc: 'Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương ' ,
-    btnIntro: ':)) lew lew ',
-    title: 'Chạy về nơi phía anh cánh tay này dài rộng lắm . Chạy về nơi phía anh , trái tim này cũng ấm lắm! 😙',
-    desc: 'Chạy đi đâu nữa em , chỉ có beenn anh là nơi an toàn nhất rồi! Đừng đi đâu , chạy về nơi phía anh ',
-    btnYes: 'Dù có gì vẫn mãi yêu anh <33',
-    btnNo: 'Không, Em chỉ coi anh là bạn :3',
-    question:'Trên thế giới này có hơn 8 tỷ nụ cười , nhưng đối với anh nụ cười của em mới là đẹp nhất <3',
+    Đằng sau chữ yêu đây là thương',
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Em sẽ yêu anh trọn đời , ngoan ngoãn để anh cưng chiều  <33333333',
-    mess: 'Anh biết mà 🥰. Anh cũng iuu iem 😘😘',
-    messDesc: 'Hẹn gặp em iu ở HANU.',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink:  ' https://m.me/v.17thsept' ,
+    messLink: 'https://m.me/v.17thsept'
 }
+
 
 
